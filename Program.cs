e4713c7bb98fdaca24076e0f8f4a6dd97e9d26cd
moElshehaby <120137569+moElshehaby@users.noreply.github.com>
2023-07-13 22:20:53 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using book_store.Data;
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddDbContext<book_storeContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("book_storeContext") ?? throw new InvalidOperationException("Connection string 'book_storeContext' not found.")));
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+builder.Services.AddSession(options => { options.IdleTimeout = TimeSpan.FromMinutes(1); });
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+}
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+//UseSession
+app.UseSession();
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=books}/{action=landing}/{id?}");
+
+app.Run();
